bc882afe08cf0a9c830ab973997f095f452a7150
Michael Mbugua <michael.mbugua@equitybank.co.ke>
2026-06-08 19:51:33 +0300

    Add Team details

--- src/app/academics/academics.ts
+++ src/app/academics/academics.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {HeroSectionComponent} from "../shared/hero-section/hero-section";
 
 @Component({
-  selector: 'app-academics',
-  standalone: true,
-  templateUrl: './academics.html',
-  styleUrl: './academics.css'
+    selector: 'app-academics',
+    standalone: true,
+    templateUrl: './academics.html',
+    imports: [
+        HeroSectionComponent
+    ],
+    styleUrl: './academics.css'
 })
 export class AcademicsComponent {}
--- src/app/board-of-trustees/board-of-trustees.ts
+++ src/app/board-of-trustees/board-of-trustees.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TimelineNavComponent } from '../shared/timeline-nav/timeline-nav';
+import {HeroSectionComponent} from "../shared/hero-section/hero-section";
 
 @Component({
   selector: 'app-board-of-trustees',
   templateUrl: './board-of-trustees.html',
   styleUrls: ['./board-of-trustees.css'],
   standalone: true,
-  imports: [TimelineNavComponent]
+  imports: [TimelineNavComponent, HeroSectionComponent]
 })
 export class BoardOfTrusteesComponent {}
