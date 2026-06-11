ae43117a0e4ab3d0fad0b72b63ae05ff1b7f062f
Michael Mbugua <michael.mbugua@equitybank.co.ke>
2026-06-11 20:04:30 +0300

    Academic page

--- src/app/academics/academics.ts
+++ src/app/academics/academics.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {HeroSectionComponent} from "../shared/hero-section/hero-section";
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-academics',
     standalone: true,
     templateUrl: './academics.html',
     imports: [
-        HeroSectionComponent
+        HeroSectionComponent,
+        RouterLink
     ],
     styleUrl: './academics.css'
 })
