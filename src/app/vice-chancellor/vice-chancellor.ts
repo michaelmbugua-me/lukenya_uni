328ae39e455d244ee60d3246ded71dba89639f99
Michael Mbugua <michael.mbugua@equitybank.co.ke>
2026-06-11 11:22:18 +0300

    Remove Home CSS issue

--- src/app/vice-chancellor/vice-chancellor.ts
+++ src/app/vice-chancellor/vice-chancellor.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { TimelineNavComponent } from '../shared/timeline-nav/timeline-nav';
-import {RouterLink} from "@angular/router";
 import { HeroSectionComponent } from '../shared/hero-section/hero-section';
+import { LeadershipMessageSectionComponent } from '../shared/leadership-message-section/leadership-message-section';
 
 @Component({
   selector: 'app-vice-chancellor',
   standalone: true,
   templateUrl: './vice-chancellor.html',
   styleUrl: './vice-chancellor.css',
-    imports: [HeroSectionComponent, TimelineNavComponent, RouterLink]
+    imports: [HeroSectionComponent, TimelineNavComponent, LeadershipMessageSectionComponent]
 })
 export class ViceChancellorComponent {}
