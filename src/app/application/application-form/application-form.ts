6e76628a6ec37d6f724ba72409d691e0c309c237
Michael Mbugua <michael.mbugua@equitybank.co.ke>
2026-06-16 12:29:20 +0300

    Update Application Overview and Form

--- src/app/application/application-form/application-form.ts
+++ src/app/application/application-form/application-form.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroSectionComponent } from '../../shared/hero-section/hero-section';
-import {StatsBandComponent} from "../../shared/stats-band/stats-band";
-import {CtaFaqComponent} from "../../shared/cta-faq/cta-faq";
 
 @Component({
   selector: 'app-application-form',
   standalone: true,
-  imports: [CommonModule, HeroSectionComponent, StatsBandComponent, CtaFaqComponent],
+  imports: [CommonModule, HeroSectionComponent],
   templateUrl: './application-form.html',
   styleUrl: './application-form.css'
 })
