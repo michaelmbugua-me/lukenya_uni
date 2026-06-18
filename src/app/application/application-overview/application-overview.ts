b65f052ebcbcf9b5f018d8f697690885228952b4
Michael Mbugua <michael.mbugua@equitybank.co.ke>
2026-06-18 13:22:28 +0300

    Update Application Overview

--- src/app/application/application-overview/application-overview.ts
+++ src/app/application/application-overview/application-overview.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { HeroSectionComponent } from '../../shared/hero-section/hero-section';
 import { CtaFaqComponent } from '../../shared/cta-faq/cta-faq';
+import { ScholarshipSectionComponent } from '../../shared/scholarship/scholarship-section';
 
 @Component({
   selector: 'app-application-overview',
   standalone: true,
-  imports: [CommonModule, RouterLink, HeroSectionComponent],
+  imports: [CommonModule, RouterLink, HeroSectionComponent, ScholarshipSectionComponent],
   templateUrl: './application-overview.html',
   styleUrl: './application-overview.css'
 })
--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -11,4 +11,5 @@ import { RouterLink } from '@angular/router';
   styleUrl: './footer.css'
 })
 export class FooterComponent {
+  currentYear = new Date().getFullYear();
 }
