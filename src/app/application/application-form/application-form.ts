2eef62dd5cd73ca3aace11420052e52a77dadedb
Michael Mbugua <michael.mbugua@equitybank.co.ke>
2026-06-18 13:28:48 +0300

    Update Application section

--- src/app/application/application-form/application-form.ts
+++ src/app/application/application-form/application-form.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroSectionComponent } from '../../shared/hero-section/hero-section';
+import { ScholarshipSectionComponent } from '../../shared/scholarship/scholarship-section';
 
 @Component({
   selector: 'app-application-form',
   standalone: true,
-  imports: [CommonModule, HeroSectionComponent],
+  imports: [CommonModule, HeroSectionComponent, ScholarshipSectionComponent],
   templateUrl: './application-form.html',
   styleUrl: './application-form.css'
 })
